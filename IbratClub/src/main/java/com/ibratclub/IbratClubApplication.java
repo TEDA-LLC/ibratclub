c6ca156e3ee01b2373ac044ee9f1bafdabe0b096
Malikov Azizjon <80214840+MalikovAzizjon4843@users.noreply.github.com>
2023-01-15 17:50:18 +0500

    create project

---
+++ IbratClub/src/main/java/com/ibratclub/IbratClubApplication.java
@@ -0,0 +1,13 @@
+package com.ibratclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbratClubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IbratClubApplication.class, args);
+    }
+
+}
---
+++ IbratClub/src/test/java/com/ibratclub/IbratClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibratclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbratClubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
