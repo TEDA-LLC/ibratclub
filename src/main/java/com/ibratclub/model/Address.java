169ec7bab72686b8848ed46dfb598b36aa4b4788
Malikov Azizjon <80214840+MalikovAzizjon4843@users.noreply.github.com>
2023-01-15 18:48:57 +0500

    address entity added

---
+++ src/main/java/com/ibratclub/model/Address.java
@@ -0,0 +1,35 @@
+package com.ibratclub.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+/**
+ * @author Malikov Azizjon  *  15.01.2023  *  18:47   *  IbratClub
+ */
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+@Builder
+@Entity
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private District district;
+
+    private String streetHome;
+
+//    private Double latitude, longitude;
+
+    public Address(District district, String streetHome) {
+        this.district = district;
+        this.streetHome = streetHome;
+    }
+
+}
