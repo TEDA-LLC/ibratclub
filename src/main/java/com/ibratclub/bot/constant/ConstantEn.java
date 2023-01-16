e9f7729f7bcb2d763849d89b1c8773082c4d22ea
Malikov Azizjon <80214840+MalikovAzizjon4843@users.noreply.github.com>
2023-01-17 00:01:26 +0500

    english constant for telegram bot

---
+++ src/main/java/com/ibratclub/bot/constant/ConstantEn.java
@@ -0,0 +1,53 @@
+package com.ibratclub.bot.constant;
+
+/**
+ * @author Malikov Azizjon  *  17.01.2023  *  0:00   *  IbratClub
+ */
+public interface ConstantEn {
+
+    String START = "Hello! Welcome to our bot! \n" +
+            "First select the service language:\n";
+    String BUTTON = "English \uD83C\uDDEC\uD83C\uDDE7";
+    String CONTACT_BUTTON = "Share contact \uD83D\uDCDE";
+    String CONTACT = "Enter your phone number\n" +
+            " \n" +
+            "Send number using <Share contact> button";
+    String ABOUT_US_BUTTON = "About us \uD83D\uDD0E";
+    String SERVICES_BUTTON = "Services \uD83D\uDEE0";
+    String SYSTEMS_BUTTON = "Systems \uD83D\uDCBB";
+    String SETTINGS_BUTTON = "Settings ⚙";
+    String VACANCY_BUTTON = "Vacancy \uD83D\uDC68\uD83C\uDFFB\u200D\uD83D\uDCBB";
+    String MENU = "Main menu : ";
+    String ABOUT_US = "<b>TEDA LLC \n \n</b>" +
+            "Improving the life of the people of Uzbekistan, which is developing today." +
+            "\n" +
+            "\n" +
+            "At the stage of the digital economy, the services of many entrepreneurs are being digitalized and offered at an affordable price.\n" +
+            "\n" +
+            "\uD83C\uDF10 <a href=\"https://teda.uz/\"> teda.uz</a>\n" +
+            "☎ <b>+998 (94) 708 04 28</b>\n" +
+            "\uD83D\uDCE7 <b>tedacompanyinfo@gmail.com</b>\n" +
+            "\uD83D\uDCCD <b>Tashkent, Dustlik metro station, Tashkent 700000</b>\n" +
+            "\uD83D\uDCF8 <a href=\"https://instagram.com/teda.uz?igshid=YmMyMTA2M2Y=\"> @teda_uz</a>\n"+
+            "<b>Leave your question to our manager and </b>\n"+
+            "<b> our manager will answer you as soon as possible.</b>\n"+
+            "\uD83D\uDC64 <b> Manager : @Proekt_Manager</b>\n" +
+            "\uD83C\uDFE6<b>Х/р: 20208000904456127001, РАКБ «QQB» Tashkent city regional branch</b>\n" +
+            "<b>МФО: 01037 INN: 205985751  ОКЭД: 62010</b>\n";
+    String VACANCY = "There are currently no vacancies\n" ;
+    //            "\uD83D\uDC64 Manager : @Proekt_Manager\n";
+    String LANGUAGE = "Change language \uD83C\uDDF7\uD83C\uDDFA/\uD83C\uDDFA\uD83C\uDDFF/\uD83C\uDDEC\uD83C\uDDE7";
+    String PHONE = "Change number \uD83D\uDCDE";
+    String BACK = "Back ⬅";
+    String CHOOSE = "Choose: ";
+    String EDITED = "Successfully modified \uD83C\uDD97";
+    String CHOOSE_LANGUAGE = "Choose language:";
+    String LANGUAGE_ICON = "ENG \uD83C\uDDEC\uD83C\uDDE7";
+    String IN_DETAIL = "In Detail\uD83D\uDD0E";
+    String REQUEST = "Request\uD83D\uDCCC";
+    String RESPONSE_FOR_REQUEST = "Your request has been successfully received.✅\n" +
+            "Our manager will contact you soon.";
+    String MY_REQUESTS = "My Requests \uD83C\uDFAF";
+    String MY_REQUESTS_EMPTY = "You don't have any requests";
+
+}
