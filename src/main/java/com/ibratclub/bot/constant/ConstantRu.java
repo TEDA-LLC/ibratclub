fcfbf9545074338709d67538123266286b3a19b8
Malikov Azizjon <80214840+MalikovAzizjon4843@users.noreply.github.com>
2023-01-17 00:16:24 +0500

    russian constant for telegram bot

---
+++ src/main/java/com/ibratclub/bot/constant/ConstantRu.java
@@ -0,0 +1,53 @@
+package com.ibratclub.bot.constant;
+
+/**
+ * @author Malikov Azizjon  *  17.01.2023  *  0:01   *  IbratClub
+ */
+public interface ConstantRu {
+
+    String START = "Здравствуйте! Добро пожаловать в наш бот! \n" +
+            "Для начала выберите язык обслуживания:\n";
+    String BUTTON = "Русский \uD83C\uDDF7\uD83C\uDDFA";
+    String CONTACT_BUTTON = "Отправить контакт \uD83D\uDCDE";
+    String CONTACT = "Введите свой номер телефона\n" +
+            " \n" +
+            "Отправить номер с помощью кнопки <ОТПРАВИТЬ КОНТАКТ>";
+    String ABOUT_US_BUTTON = "О нас \uD83D\uDD0E";
+    String SERVICES_BUTTON = "Услуги \uD83D\uDEE0";
+    String SYSTEMS_BUTTON = "Системы \uD83D\uDCBB";
+    String SETTINGS_BUTTON = "Настройки ⚙";
+    String VACANCY_BUTTON = "Вакансия \uD83D\uDC68\uD83C\uDFFB\u200D\uD83D\uDCBB";
+    String MENU = "Главное меню : ";
+    String ABOUT_US = "<b>TEDA LLC \n \n</b>" +
+            "Улучшение жизни народа Узбекистана, которая развивается сегодня." +
+            "\n" +
+            "На этапе цифровой экономики оцифровка услуг многих предпринимателей и предложение их по доступной цене.\n" +
+            "\n" +
+            "\uD83C\uDF10 <a href=\"https://teda.uz/\"> teda.uz</a>\n" +
+            "☎ <b>+998 (94) 708 04 28</b>\n" +
+            "\uD83D\uDCE7 <b>tedacompanyinfo@gmail.com</b>\n" +
+            "\uD83D\uDCCD <b>Г. Ташкент, Дустлик метро, Тошкент 700000</b>\n" +
+            "\uD83D\uDCF8 <a href=\"https://instagram.com/teda.uz?igshid=YmMyMTA2M2Y=\"> @teda_uz</a>\n"+
+            "<b>Оставьте свой вопрос нашему менеджеру и</b>\n"+
+            "<b> наш менеджер ответит вам в ближайшее время.</b>\n"+
+            "\uD83D\uDC64 <b>Менеджер : @Proekt_Manager</b>\n" +
+            "\uD83C\uDFE6<b>Х/р: 20208000904456127001, РАКБ «ҚИШЛОҚҚУРИЛИШБАНК» Тошкент шаҳар регионал ф-ли</b>\n" +
+            "<b>МФО: 01037 ИНН: 205985751  ОКЭД: 62010</b>\n";
+    String VACANCY = "Вакансий пока нет \n" ;
+    //            "\uD83D\uDC64 Менеджер : @Proekt_Manager\n";
+    String LANGUAGE = "Изменить язык \uD83C\uDDF7\uD83C\uDDFA/\uD83C\uDDFA\uD83C\uDDFF/\uD83C\uDDEC\uD83C\uDDE7";
+    String PHONE = "Изменить номер \uD83D\uDCDE";
+    String BACK = "Назад ⬅";
+    String CHOOSE = "Выберите: ";
+    String EDITED = "Успешно изменен \uD83C\uDD97";
+    String CHOOSE_LANGUAGE = "Пожалуйста выберите язык:";
+    String LANGUAGE_ICON = "RUS \uD83C\uDDF7\uD83C\uDDFA";
+    String IN_DETAIL = "Подробно\uD83D\uDD0E";
+    String REQUEST = "Заявка\uD83D\uDCCC";
+    String RESPONSE_FOR_REQUEST= "Ваш запрос успешно получен.✅\n" +
+            "Наш менеджер свяжется с вами в ближайшее время.";
+    String MY_REQUESTS = "Мои Заявки \uD83C\uDFAF";
+
+    String MY_REQUESTS_EMPTY = "У вас пока нет заявки";
+
+}
