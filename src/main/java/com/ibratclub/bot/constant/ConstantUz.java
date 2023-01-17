629417b76f9852f5dd9df2089c20204034f74b5b
Malikov Azizjon <80214840+MalikovAzizjon4843@users.noreply.github.com>
2023-01-17 22:00:07 +0500

    uzbek constant for telegram bot

---
+++ src/main/java/com/ibratclub/bot/constant/ConstantUz.java
@@ -0,0 +1,55 @@
+package com.ibratclub.bot.constant;
+
+/**
+ * @author Malikov Azizjon  *  17.01.2023  *  21:59   *  IbratClub
+ */
+public interface ConstantUz {
+
+    String START = "Assalomu aleykum! Botimizga xush kelibsiz! \n" +
+            "Xizmat ko’rsatish tilini tanlang:\n";
+    String BUTTON = "O'zbekcha \uD83C\uDDFA\uD83C\uDDFF";
+    String CONTACT_BUTTON = "Raqamni jo'natish \uD83D\uDCDE";
+    String CONTACT = "Telefon raqamingizni kiriting\n" +
+            " \n" +
+            "<RAQAMNI JO'NATISH> tugmasi yordamida raqamni yuboring";
+    String ABOUT_US_BUTTON = "Biz haqimizda \uD83D\uDD0E";
+    String SERVICES_BUTTON = "Xizmatlar \uD83D\uDEE0";
+    String SYSTEMS_BUTTON = "Tizimlar \uD83D\uDCBB";
+    String SETTINGS_BUTTON = "Sozlamalar ⚙";
+    String VACANCY_BUTTON = "Vakansiya \uD83D\uDC68\uD83C\uDFFB\u200D\uD83D\uDCBB";
+    String MENU = "Asosiy menyu : ";
+    String ABOUT_US = "<b>TEDA LLC \n \n</b>" +
+            "Hozirgi kunda rivojlanib borayotgan O'zbekiston xalqining hayotini yaxshilash." +
+            "\n" +
+            "Raqamli Iqtisodiyot bosqichida ko'plab tadbirkorlarning xizmatlarini elektronlashtirish va ularni qulay narxda taklif qilish.\n" +
+            "\n" +
+            "\uD83C\uDF10 <a href=\"https://teda.uz/\"> teda.uz</a>\n" +
+            "☎ <b>+998 (94) 708 04 28</b>\n" +
+            "\uD83D\uDCE7 <b>tedacompanyinfo@gmail.com</b>\n" +
+            "\uD83D\uDCCD <b>Toshkent sh. Do'stlik metro ,Toshkent 700000</b>\n" +
+            "\uD83D\uDCF8 <a href=\"https://instagram.com/teda.uz?igshid=YmMyMTA2M2Y=\"> @teda_uz</a>\n" +
+            "<b>Menejirimizga savolingizni qoldiring va </b>\n"+
+            "<b> menejerimiz sizga imkon qadar tezroq javob beradi.</b>\n"+
+            "\uD83D\uDC64 <b> Manager : @Proekt_Manager</b>\n" +
+            "\uD83C\uDFE6<b>Х/р: 20208000904456127001, РАКБ «QQB» Toshkent shahar regional f-li</b>\n" +
+            "<b>МФО: 01037 INN: 205985751  ОКЭД: 62010</b>\n";
+
+    String VACANCY = "Hozircha vakansiya yo`q \n" ;
+    //            "\uD83D\uDC64 Manager : @Proekt_Manager\n";
+    String LANGUAGE = "Tilni o'zgartirish \uD83C\uDDF7\uD83C\uDDFA/\uD83C\uDDFA\uD83C\uDDFF/\uD83C\uDDEC\uD83C\uDDE7";
+    String PHONE = "Raqamni o'zgartirish \uD83D\uDCDE";
+    String BACK = "Orqaga ⬅";
+    String CHOOSE = "Tanlang: ";
+    String EDITED = "Muvafaqiyatli o'zgartirildi \uD83C\uDD97";
+    String CHOOSE_LANGUAGE = "Iltimos tilni tanlang:";
+    String LANGUAGE_ICON = "UZB \uD83C\uDDFA\uD83C\uDDFF";
+    String OK = "\uD83C\uDD97";
+    String IN_DETAIL = "Batafsil\uD83D\uDD0E";
+    String REQUEST = "So'rov\uD83D\uDCCC";
+    String RESPONSE_FOR_REQUEST = "Murojaatingiz muvafaqiyatli qabul qilindi.✅\n" +
+            "Tez orada menejerimiz aloqaga chiqadi.";
+    String MY_REQUESTS = "Mening Murojaatlarim \uD83C\uDFAF";
+
+    String MY_REQUESTS_EMPTY = "Sizda hali hech qanday murojatlar mavjud emas";
+
+}
