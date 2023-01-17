3ed56472d23d9bcd7253bc3d8a6b200827f8ff3d
Malikov Azizjon <80214840+MalikovAzizjon4843@users.noreply.github.com>
2023-01-17 23:05:27 +0500

    order controller

---
+++ src/main/java/com/ibratclub/controller/OrderController.java
@@ -0,0 +1,15 @@
+package com.ibratclub.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Malikov Azizjon  *  17.01.2023  *  23:04   *  IbratClub
+ */
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+}
