bb7d898a1819c87abbb2994cd81d759c260fe741
Malikov Azizjon <80214840+MalikovAzizjon4843@users.noreply.github.com>
2023-01-17 22:58:25 +0500

    ready pre filter security config

---
+++ src/main/java/com/ibratclub/config/PreFilter.java
@@ -0,0 +1,44 @@
+package com.ibratclub.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+/**
+ * @author Malikov Azizjon  *  17.01.2023  *  22:23   *  IbratClub
+ */
+
+@Component
+@RequiredArgsConstructor
+public class PreFilter extends OncePerRequestFilter {
+
+    @Value("${telegram.bot.token}")
+    String botToken;
+
+    @SneakyThrows
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) {
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Access-Control-Allow-Headers", "*");
+        response.addHeader("Access-Control-Allow-Methods", "*");
+        response.addHeader("Access-Control-Allow-Credentials", "true");
+        String token = request.getHeader("Authorization");
+        if (token == null || token.length() <= 8 || !token.substring(7).equals(botToken)) {
+            if (!request.getServletPath().startsWith("/api/site") && !request.getServletPath().startsWith("/api/vacancy") && !request.getMethod().equals("GET")&& !request.getMethod().equals("POST") ){ //&& !request.getMethod().equals("POST") && !request.getMethod().equals("GET") && !request.getMethod().equals("PUT")) {
+                response.getWriter().print("{\"message\":\"Forbidden!\",\"success\":false,\"status\":403}");
+                response.setContentType("application/json");
+                if (!request.getMethod().equalsIgnoreCase("OPTIONS")) {
+                    response.setStatus(403);
+                }
+                return;
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+}
