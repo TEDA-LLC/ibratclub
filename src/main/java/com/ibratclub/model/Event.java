36d2b35ad4212032cf3307085d803cb9434adb46
Malikov Azizjon <80214840+MalikovAzizjon4843@users.noreply.github.com>
2023-01-23 23:27:53 +0500

    event entity added

---
+++ src/main/java/com/ibratclub/model/Event.java
@@ -0,0 +1,38 @@
+package com.ibratclub.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+/**
+ * @author Malikov Azizjon  *  23.01.2023  *  23:20   *  IbratClub
+ */
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private String organizer;
+
+    @JsonFormat(pattern="yyyy-MM-dd")
+    private LocalDateTime dateTime;
+
+    private String contact;
+
+    @ManyToOne
+    private Address eventAddress;
+
+}
